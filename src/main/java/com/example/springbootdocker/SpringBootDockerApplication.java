301be4b40da4118bed7f754054391c3024ba4bc7
Duncan N. <duncan.osindi@gmail.com>
2025-03-31 00:51:38 +0300

    clean up

--- src/main/java/com/example/springbootdocker/SpringBootDockerApplication.java
+++ src/main/java/com/example/springbootdocker/SpringBootDockerApplication.java
@@ -8,7 +8,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
-@RestController
 @SpringBootApplication
 public class SpringBootDockerApplication {
 
